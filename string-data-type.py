bf60251201ac9fe93783ec4254935852b166ec39
mittal-jn <mittal.lunkad@gmail.com>
2025-09-01 23:19:22 -0500

    Uploading Python Programming scripts

---
+++ string-data-type.py
@@ -0,0 +1,24 @@
+myString = " This is a string. "
+
+print(myString)
+
+print(type(myString))
+
+print(myString + " is the data type" + str(type(myString)))
+
+firstString = "water"
+
+secondString = "fall"
+
+thirdString = firstString + secondString
+
+print(thirdString)
+
+name = input("What is your name? ")
+
+print(name)
+
+color= input("what is your favourite color ? ")
+animal = input("what is your favourite animal?")
+
+print("{}, you like a {} {} !" .format(name,color,animal))
